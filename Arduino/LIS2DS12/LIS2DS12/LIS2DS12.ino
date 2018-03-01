3923ef2709531916e423c2ea0b60bbd8368d5ac8
David Mann <dmann@loggerhead.com>
2018-03-01 15:21:20 -0500

    SPI interface

--- Arduino/LIS2DS12/LIS2DS12/LIS2DS12.ino
+++ Arduino/LIS2DS12/LIS2DS12/LIS2DS12.ino
@@ -2,11 +2,8 @@
 // I2C interface
 
 // - setup 2 MHz SPI read of LIS2 -- 5x faster than I2C
-// - setup watermark
 // - write data to sd card and benchmark
 // - sleep mode and watermark to wake
-// - change to use magnitude and benchmark
-
 
 #include <SPI.h>
 #include <SdFat.h>
@@ -21,7 +18,7 @@
 //
 //#include <SoftI2CMaster.h>
 
-boolean saveData = 1;  // set to 1 to save data to microSD; for debugging
+boolean saveData = 0;  // set to 1 to save data to microSD; for debugging
 
 // pin assignments
 #define chipSelect 10
@@ -31,6 +28,11 @@ boolean saveData = 1;  // set to 1 to save data to microSD; for debugging
 #define VHFPOW 9
 #define BUTTON1 A2
 
+#define DATAOUT 11      //MOSI
+#define DATAIN 12       //MISO
+#define SPICLOCK 13      //sck
+#define chipSelectPin 3 //Hall pin
+
 volatile int accel[32];
 
 // SD file system
@@ -39,35 +41,46 @@ File dataFile;
 
 void setup() {
   Serial.begin(115200);
-  delay(4000);
+  delay(2000);
   Serial.println("LIS2DS12");
 
   pinMode(LED_GRN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
-  digitalWrite(LED_RED,LOW);
-  digitalWrite(LED_GRN,HIGH);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GRN, HIGH);
 
-  Wire.begin();
-  Wire.setClock(400000);
+  //  Wire.begin();
+  //  Wire.setClock(400000);
 
-//  byte I2C_check = i2c_init();
-//  if(I2C_check == false){
-//    Serial.println("I2C Init Failed--SDA or SCL may not be pulled up!");
-//     while(1){
-//       flashRed();
-//     }
-//  }
+  //  byte I2C_check = i2c_init();
+  //  if(I2C_check == false){
+  //    Serial.println("I2C Init Failed--SDA or SCL may not be pulled up!");
+  //     while(1){
+  //       flashRed();
+  //     }
+  //  }
+  
+  // initalize the  data ready and chip select pins:
+  pinMode(chipSelectPin, OUTPUT);
+  digitalWrite(chipSelectPin, HIGH);
+  pinMode(SPICLOCK, OUTPUT);
+  pinMode(DATAOUT, OUTPUT);
+  pinMode(DATAIN, INPUT);
 
-  int testResponse = lis2TestResponse();
-  if(testResponse != 67){
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0)); // with breadboard, speeds higher than 1MHz fail
+  // might get better when chip on board with short traces
+  
+  int testResponse = lis2SpiTestResponse();
+  if (testResponse != 67) {
     Serial.println("Accel init failed");
-    while(1){
+    while (1) {
       flashRed();
     }
   }
 
-  if(saveData){
-      Serial.println("Init microSD");
+  if (saveData) {
+    Serial.println("Init microSD");
     // see if the card is present and can be initialized:
     while (!sd.begin(chipSelect, SPI_FULL_SPEED)) {
       Serial.println("Card failed");
@@ -76,39 +89,40 @@ void setup() {
     fileInit();
   }
 
-  
-  Serial.println(testResponse); 
-  lis2Init();
+
+  Serial.println(testResponse);
+  lis2SpiInit();
 }
 
 int count = 0;
 void loop() {
- while(count < 5000){
-   if(lis2FifoStatus()>0){
-    count++;
-    lis2FifoRead(24);  //bytes to read (Wire library has max of 32)
-    if(saveData) {
-      for (int i=0; i<24; i+=2){
-        dataFile.println((int) accel[i+1]<<8 | accel[i]);
+  while (count < 5000) {
+    if (lis2SpiFifoStatus() > 0) {
+      count++;
+      lis2SpiFifoRead(24);  //bytes to read (Wire library has max of 32)
+      Serial.println((int) accel[1]<<8 | accel[0]);
+      if (saveData) {
+        for (int i = 0; i < 24; i += 2) {
+          dataFile.println((int) accel[i + 1] << 8 | accel[i]);
+        }
       }
     }
-   }
- }
- if (saveData) dataFile.close();
- Serial.println("Done.");
- while(1);
+  }
+  if (saveData) dataFile.close();
+  Serial.println("Done.");
+  while (1);
 }
 
-void flashRed(){
-   digitalWrite(LED_RED, HIGH);
-   delay(500);
-   digitalWrite(LED_RED, LOW);
-   delay(500);
+void flashRed() {
+  digitalWrite(LED_RED, HIGH);
+  delay(500);
+  digitalWrite(LED_RED, LOW);
+  delay(500);
 }
 
-void fileInit(){
+void fileInit() {
   char filename[12];
-  sprintf(filename,"test.csv");  //filename is DDHHMM
+  sprintf(filename, "test.csv"); //filename is DDHHMM
   dataFile = sd.open(filename, O_WRITE | O_CREAT | O_APPEND);
 }
 
